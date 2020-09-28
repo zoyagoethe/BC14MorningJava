ddd10c458b0880f6c4ad4a72f2303c7c4ad7ae9e
andron13 <andron13@gmail.com>
2020-09-28 10:48:30 +0200

    count

--- src/day15/LovelyLeapYear.java
+++ src/day15/LovelyLeapYear.java
@@ -5,11 +5,23 @@ public class LovelyLeapYear {
     public static void main(String[] args) {
         //printLeapYears(2000, 2009);
         //printLeapYears(1900, 1967);
+        countLeapYears(1900, 2009);
+    }
+
+    private static void countLeapYears(int year1, int year2) {
+        int count = 0;
+        for (int i = year1; i <= year2; i++) {
+            if (checkLeapYear(i)) {
+                count++;
+            }
+        }
+        System.out.println("Между годами " + year1 + " и "
+                + year2 + " всего " + count + " високосных лет");
     }
 
     private static void printLeapYears(int year1, int year2) {
         for (int i = year1; i <= year2; i++) { //>
-            if(checkLeapYear(i)){
+            if (checkLeapYear(i)) {
                 System.out.println(i);
             }
         }
