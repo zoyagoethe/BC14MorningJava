dcbd551aef76695d8a3a50d2742ffd35e59cf8bb
andron13 <andron13@gmail.com>
2020-10-05 12:31:53 +0200

    min. max. middle, swap

---
+++ src/day20/Exercises08_01.java
@@ -0,0 +1,11 @@
+package day20;
+
+public class Exercises08_01 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int max;
+        int min;
+        int middle;
+        int sum;
+    }
+}
---
+++ src/day20/SwapVariable.java
@@ -0,0 +1,13 @@
+package day20;
+
+public class SwapVariable {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+    }
+}
