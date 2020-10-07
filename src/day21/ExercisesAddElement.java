cd57e4e764ce67e7e8b9bc1535d8a4a6303c8796
andron13 <andron13@gmail.com>
2020-10-07 10:12:00 +0200

    add element into array

--- src/day21/ExercisesAddElement.java
+++ src/day21/ExercisesAddElement.java
@@ -1,5 +1,7 @@
 package day21;
 
+import day20.FillAndPrintIntArray;
+
 public class ExercisesAddElement {
     public static void main(String[] args) {
         int[] customerArray = new int[]{1, 2, 3, 4, 5, 6, 7};
@@ -10,15 +12,21 @@ public class ExercisesAddElement {
         int element = 10000;
         // key, value
         // addElement(customerArray, key, value);
+        FillAndPrintIntArray.myPrintIntArray(addElement(customerArray, key, value));
     }
 
-/*    private static int[] addElement(int[] input, int index, int value) {
+    private static int[] addElement(int[] input, int index, int value) {
         int[] result = new int[input.length + 1];
+        // 1. шаг
         for (int i = 0; i < index; i++) {
             result[i] = input[i];
         }
-        for (int i = index + 1; i < input.length; i++) {
-            result[i] = input[i];
+        // 2. шаг
+        result[index] = value;
+        // 3. шаг
+        for (int i = index + 1; i < result.length; i++) {
+            result[i] = input[i - 1];
         }
-    }*/
+        return result;
+    }
 }
