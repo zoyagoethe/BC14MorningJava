9adde03bcb1a1431cb6760a122101f430de1520d
andron13 <andron13@gmail.com>
2020-10-08 12:01:45 +0200

    table

--- src/day23/MultiDimensionalArrays.java
+++ src/day23/MultiDimensionalArrays.java
@@ -2,7 +2,42 @@ package day23;
 
 public class MultiDimensionalArrays {
     public static void main(String[] args) {
-        magicMonth();
+        //magicMonth();
+        simpleTable();
+    }
+
+    private static void simpleTable() {
+        String[][] myArray = new String[5][3];
+
+        myArray[0][0] = "1";
+        myArray[0][1] = "Матвеев";
+        myArray[0][2] = "№3482394832";
+
+        myArray[1][0] = "2";
+        myArray[1][1] = "Сидоров";
+        myArray[1][2] = "№34564532";
+
+        myArray[2][0] = "3";
+        myArray[2][1] = "Петров";
+        myArray[2][2] = "№432832";
+
+        myArray[3][0] = "4";
+        myArray[3][1] = "Матвеев";
+        myArray[3][2] = "№3482394832";
+
+        myArray[4][0] = "5";
+        myArray[4][1] = "Васечкин";
+        myArray[4][2] = "№879679";
+        printTwoDStringArray(myArray);
+    }
+
+    private static void printTwoDStringArray(String[][] input) {
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length ; j++) {
+                System.out.print(input[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
     private static void magicMonth() {
