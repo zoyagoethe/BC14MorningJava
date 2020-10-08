027da792e9035175542e590b9cdf87ff583e8f76
andron13 <andron13@gmail.com>
2020-10-08 12:17:25 +0200

    md array

--- src/day23/MultiDimensionalArrays.java
+++ src/day23/MultiDimensionalArrays.java
@@ -3,7 +3,56 @@ package day23;
 public class MultiDimensionalArrays {
     public static void main(String[] args) {
         //magicMonth();
-        simpleTable();
+        //simpleTable();
+        //anotherArray();
+        //arrayFor();
+        arrayFive();
+    }
+
+    private static void arrayFive() {
+        int[][] example = {
+                {1},
+                {2, 2},
+                {3, 3, 3},
+                {4, 4, 4, 4},
+                {5, 5, 5, 5, 5},
+        };
+    }
+
+    private static void arrayFor() {
+        int[][] a1 = new int[5][];
+        a1[0] = new int[1];
+        a1[1] = new int[20];
+        a1[2] = new int[0];
+        a1[3] = new int[7];
+        a1[4] = new int[5];
+        for (int[] element : a1) {
+            for (int elementik : element) {
+                System.out.print(elementik + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void anotherArray() {
+        int[][][] a1 = new int[3][4][5];
+        byte[][][] byteArray = {
+                {
+                        {0, 1, 2},
+                        {3, 4, 5},
+                        {6, 7, 8},
+                },
+                {
+                        {0, 1, 2},
+                        {3, 4, 5},
+                        {6, 7, 8},
+                },
+                {
+                        {0, 1, 2},
+                        {3, 4, 5},
+                        {6, 7, 8},
+                }
+        };
     }
 
     private static void simpleTable() {
@@ -33,7 +82,7 @@ public class MultiDimensionalArrays {
 
     private static void printTwoDStringArray(String[][] input) {
         for (int i = 0; i < input.length; i++) {
-            for (int j = 0; j < input[i].length ; j++) {
+            for (int j = 0; j < input[i].length; j++) {
                 System.out.print(input[i][j] + " ");
             }
             System.out.println();
