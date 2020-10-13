43e9a5e66e2a422844683a1b381805103b0b48b0
Anna Freimuth <a.freimuth90@gmail.com>
2020-10-13 19:04:10 +0200

    Added homework 26.

---
+++ src/day26_homework/Task_9.java
@@ -0,0 +1,15 @@
+package day26_homework;
+
+public class Task_9 {
+    public static void main(String[] args) {
+        System.out.println(endOther("Hiabc", "abc")); // → true
+        System.out.println(endOther("AbC", "HiaBc")); //→ true
+        System.out.println(endOther("abc", "abXabc"));// → true
+        System.out.println(endOther("abcZ", "abXabc"));// → false
+    }
+
+    private static boolean endOther(String str1, String str2) {
+        return str2.toLowerCase().endsWith(str1.toLowerCase())
+                || str1.toLowerCase().endsWith(str2.toLowerCase());
+    }
+}
