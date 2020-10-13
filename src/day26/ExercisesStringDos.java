ea5b9940f3f5f4adfa3bd5cef33588e8165f7642
andron13 <andron13@gmail.com>
2020-10-13 13:05:09 +0200

    ex

--- src/day26/ExercisesStringDos.java
+++ src/day26/ExercisesStringDos.java
@@ -2,14 +2,32 @@ package day26;
 
 public class ExercisesStringDos {
     public static void main(String[] args) {
-        System.out.println(wordEnds("abcXY123XYijk", "XY"));// →"c13i"
+       /* System.out.println(wordEnds("abcXY123XYijk", "XY"));// →"c13i"
         System.out.println(wordEnds("XY123XY", "XY"));// →"13"
         System.out.println(wordEnds("XY1XY", "XY"));// →"11"
-        System.out.println(wordEnds("XY1XY", "YX"));//
+        // System.out.println(wordEnds("XY1XY", "XY"));//
         // wordstr -  2 simwola
         // 1. Найти место (любое) в первом стринге, где встречается наш патерн
         // 2. Обращаемся к элементам до и элементам после
-        // Конкатинируем нуженые элементы.
+        // Конкатинируем нуженые элементы.*/
+
+        System.out.println(sameStarChar("xy*yzz")); //→ true
+        System.out.println(sameStarChar("xy*zzz")); //→ false
+        System.out.println(sameStarChar("*xa*az")); //→ true  false
+    }
+
+    private static boolean sameStarChar(String input) {
+        char star = '*';
+        for (int i = 0; i < input.length(); i++) {
+            if ((i == 0 || i == input.length() - 1)
+                    && input.charAt(i) == star) {
+                return false;
+            } else if (input.charAt(i) == star
+                    && input.charAt(i - 1) != input.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static String wordEnds(String str1, String wordStr) {
