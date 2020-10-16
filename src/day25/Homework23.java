7f33707e8106839a0cf83956b1aa4465835b92a4
Zoya Göthe <zoya.goethe13@gmail.com>
2020-10-16 10:23:27 +0200

    try and cry

--- src/day25/Homework23.java
+++ src/day25/Homework23.java
@@ -1,45 +1,37 @@
 package day25;
 
-public class Homework23 {
+public class HomeWork23 {
     public static void main(String[] args) {
-        System.out.println(repeatSeparator("Word", "X", 3));// → "WordXWordXWord"
-        System.out.println(repeatSeparator("This", "And", 2));// → "ThisAndThis"
-        System.out.println(repeatSeparator("This", "And", 1));// → "This"
-
-        //System.out.println(getSandwich("breadjambread"));// → "jam"
-        //System.out.println(getSandwich("xxbreadjambreadyy"));// → "jam"
-        //System.out.println(getSandwich("xxbreadyy"));// → ""
-
-        System.out.println(zipZap("zipXzap"));// → "zpXzp"
-        System.out.println(zipZap("zopzop"));// → "zpzp"
-        System.out.println(zipZap("zzzopzop"));// → "zzzpzp"
+      /*  System.out.println(repeatSeparator("Word", "X", 3));
+        System.out.println(repeatSeparator("This", "And", 2));
+        System.out.println(repeatSeparator("This", "And", 1));
+*/
+        System.out.println(getSandwich("breadjambread"));// → "jam"
+        System.out.println(getSandwich("xxbreadjambreadyy"));// → "jam"
+        System.out.println(getSandwich("xxbreadyy"));//     ""
     }
 
-    private static String zipZap(String str) {
-        String output = str;
-        for (int i = 1; i < output.length() - 1; i++) {
-            if (output.charAt(i - 1) == 'z' && output.charAt(i + 1) == 'p') {
-                output = output.substring(0, i) + output.substring(i + 1);
-            }
-        }
-        return output;
-    }
-
-    private static String getSandwich(String input) {
+    private static String getSandwich(String str) {
         String output = "";
         int indexStart = 0;
-        String word = "bread";
-        indexStart = input.indexOf(word);
-        int indexEnd = input.lastIndexOf(word);
+        String bread = "bread";
+        indexStart = str.indexOf(bread);
+        // System.out.println("indexStart= "+str.indexOf(bread));
+        int indexEnd = str.lastIndexOf(bread);
+        //System.out.println("indexend= "+indexEnd);пше ыефегы
+
+        if (indexEnd != indexStart) {
+            output = str.substring(indexStart + bread.length(), indexEnd);
+        }
         return output;
     }
 
-    private static String repeatSeparator(String word, String sep, int number) {
-        String output = "";
-        for (int i = 0; i < number - 1; i++) {
-            output += word + sep;
+    private static String repeatSeparator(String str1, String str2, int numer) {
+        String outputStr = "";
+        for (int i = 0; i < numer - 1; i++) {
+            outputStr += str1 + str2;
         }
-        output += word;
-        return output;
+        outputStr += str1;
+        return outputStr;
     }
 }
